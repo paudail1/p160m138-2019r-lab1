85f2cebed7771589ddb2fcc8c8b8d98959cfd4cb
pdailidonis <paudail1@ktu.lt>
2019-10-28 19:38:54 +0000

    keliu branch i master saka

--- Hello.py
+++ Hello.py
@@ -1,9 +1,11 @@
 import sys
 
-name = sys.argv[1]
 
-def greet (name: str):
-    return f"Hello, {name}!"
+def greet(name: str, shout_count: int = 1):
+    return f"Hello, {name}{shout_count * '!'}"
 
-print (greet(name))
 
+if __name__ == "__main__":
+
+    n = int(sys.argv[2]) if len(sys.argv) == 3 else 1
+    print(greet(sys.argv[1], n))
